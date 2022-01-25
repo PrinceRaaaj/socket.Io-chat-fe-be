3ce055b2bfb8dc444718dd575230d1249c3e23e7
Prince Raj <princefebraj08@gmail.com>
2022-01-25 09:07:29 +0530

    port changed

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ io.on("connection", (socket) => {
 });
 
 
-
-server.listen(8000, () => console.log("app started at port 8000"))
+const port = process.env.PORT || 8000
+server.listen(port, () => console.log(`app started at port ${port}`))
